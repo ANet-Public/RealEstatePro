f158751dfec6a9314cf59e64568b8fc143a14e0e
Angle <135405684+Tolib-Angle@users.noreply.github.com>
2026-03-14 19:34:09 +0500

    [feature] Added project modules and init project [#1] (#2)
    * [feature] Added project modules and init project [#1]

---
+++ RealEstatePro.Api/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// For example, a simple endpoint to check if the API is running
+app.MapGet("/", () => "RealEstatePro API is running");
+
+app.Run();
