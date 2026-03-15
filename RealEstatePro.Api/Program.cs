5f9d254432fae569d14db247514d7171e5b1bddb
Angle <135405684+Tolib-Angle@users.noreply.github.com>
2026-03-15 20:36:32 +0500

    [feature] Added DI configure, swagger api and health endpoint [#8] (#9)

--- RealEstatePro.Api/Program.cs
+++ RealEstatePro.Api/Program.cs
@@ -1,26 +1,23 @@
+/*
+    Copyright (c) 2026 ANet-Public
+    All rights reserved.
+ */
+
+using RealEstatePro.Api.Extensions;
+using RealEstatePro.Application.DependencyInjection;
+using RealEstatePro.Infrastructure.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services
+    .AddApi()
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-// For example, a simple endpoint to check if the API is running
-app.MapGet("/", () => "RealEstatePro API is running");
+app.UseApiPipeline();
 
 app.Run();
+
+public partial class Program;
---
+++ RealEstatePro.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿/*
+    Copyright (c) 2026 ANet-Public
+    All rights reserved.
+ */
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RealEstatePro.Infrastructure.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddCoreServices(configuration);
+
+            return services;
+        }
+
+        private static IServiceCollection AddCoreServices(this IServiceCollection services, IConfiguration configuration)
+        {       
+            return services;
+        }
+    }
+}
